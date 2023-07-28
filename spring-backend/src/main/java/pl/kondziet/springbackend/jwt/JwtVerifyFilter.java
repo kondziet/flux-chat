22f0f53a5d53b988597b4f7f42f338b16f0cd154
kondziet <konrad.dabrowski54@gmail.com>
2023-07-29 01:25:50 +0200

    Inject UserRepository

--- spring-backend/src/main/java/pl/kondziet/springbackend/jwt/JwtVerifyFilter.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/jwt/JwtVerifyFilter.java
@@ -1,5 +1,6 @@
 package pl.kondziet.springbackend.jwt;
 
+import io.jsonwebtoken.security.SignatureException;
 import lombok.AllArgsConstructor;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -7,11 +8,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
+import pl.kondziet.springbackend.repository.UserRepository;
 import reactor.core.publisher.Mono;
 
 @AllArgsConstructor
@@ -21,6 +24,8 @@ public class JwtVerifyFilter implements WebFilter {
 
     private final JwtService jwtService;
     private final ReactiveUserDetailsService userDetailsService;
+    private final UserRepository userRepository;
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
 //        System.out.println("REQUEST ARRIVED");
@@ -31,12 +36,19 @@ public class JwtVerifyFilter implements WebFilter {
             String userEmail = jwtService.extractUserEmail(token);
 
             if (StringUtils.hasText(userEmail)) {
-                System.out.printf("Bearer token: %s Email: %s\n", token, userEmail);
-
-                Authentication authentication = new UsernamePasswordAuthenticationToken(userEmail, null);
-                return ReactiveSecurityContextHolder.getContext()
-                        .doOnNext(securityContext -> securityContext.setAuthentication(authentication))
-                        .then(chain.filter(exchange));
+                return userRepository.findByEmail(userEmail)
+                        .map(userDetails -> jwtService.isTokenValid(token, userDetails))
+                        .flatMap(isValid -> {
+                           if (isValid) {
+                               System.out.printf("Bearer token: %s Email: %s\n", token, userEmail);
+                               Authentication authentication = new UsernamePasswordAuthenticationToken(userEmail, null);
+                               return ReactiveSecurityContextHolder.getContext()
+                                       .doOnNext(securityContext -> securityContext.setAuthentication(authentication))
+                                       .then(chain.filter(exchange));
+                           } else {
+                               return Mono.empty();
+                           }
+                        });
             }
         }
 
