634ee5bc0db672dd9488ae40549da7dd5f940b37
kondziet <konrad.dabrowski54@gmail.com>
2023-07-29 01:26:01 +0200

    Update DataSeed

--- spring-backend/src/main/java/pl/kondziet/springbackend/util/DataSeed.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/util/DataSeed.java
@@ -4,7 +4,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import pl.kondziet.springbackend.model.entity.Message;
 import pl.kondziet.springbackend.model.entity.User;
+import pl.kondziet.springbackend.model.enumerable.MessageType;
+import pl.kondziet.springbackend.repository.MessageRepository;
 import pl.kondziet.springbackend.repository.UserRepository;
 import reactor.core.publisher.Mono;
 
@@ -13,6 +16,7 @@ import reactor.core.publisher.Mono;
 public class DataSeed implements CommandLineRunner {
 
     private final UserRepository userRepository;
+    private final MessageRepository messageRepository;
     private final PasswordEncoder passwordEncoder;
     @Override
     public void run(String... args) throws Exception {
@@ -24,6 +28,16 @@ public class DataSeed implements CommandLineRunner {
 
         userRepository.save(user1).subscribe(savedUser -> {
             System.out.println("User saved successfully: " + savedUser);
+        });
+
+        Message message1 = Message.builder()
+                .type(MessageType.CHAT)
+                .content("Hello guys :)")
+                .sender("Mike")
+                .build();
+
+        messageRepository.save(message1).subscribe(savedMessage -> {
+            System.out.println("Message saved successfully: " + savedMessage);
         });
     }
 }
