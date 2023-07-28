024cf54700ac317f39330969771df1401bf7ec7b
kondziet <konrad.dabrowski54@gmail.com>
2023-07-29 01:25:20 +0200

    Create message details which is message entity data aggregation

---
+++ spring-backend/src/main/java/pl/kondziet/springbackend/model/aggregation/MessageDetails.java
@@ -0,0 +1,13 @@
+package pl.kondziet.springbackend.model.aggregation;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDetails {
+
+    private String senderNickName;
+}
