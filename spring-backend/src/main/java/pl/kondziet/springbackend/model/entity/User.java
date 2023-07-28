1dec3964dc774b93fdd2e604b07419481336858f
kondziet <konrad.dabrowski54@gmail.com>
2023-07-29 01:23:38 +0200

    Make user email unique

--- spring-backend/src/main/java/pl/kondziet/springbackend/model/entity/User.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/model/entity/User.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -24,6 +25,7 @@ public class User implements UserDetails {
     @Id
     private String id;
     private String nickName;
+    @Indexed(unique = true)
     private String email;
     private String password;
 
