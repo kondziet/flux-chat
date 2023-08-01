6b5c7934e23a24d6efb17b1a2120d477553a4df1
kondziet <konrad.dabrowski54@gmail.com>
2023-08-01 22:00:45 +0200

    Render AuthenticationPage

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AuthenticationPage from './pages/AuthenticationPage';
+import { AuthenticationProvider } from './context/AuthenticationContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthenticationProvider>
+    <div className="h-screen">
+      <AuthenticationPage />
+      {/* <App /> */}
+    </div>
+  </AuthenticationProvider>
 );
