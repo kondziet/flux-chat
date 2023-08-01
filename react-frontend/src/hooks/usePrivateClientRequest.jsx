064da927d5fbc865e95c47d822ab0bf413e9e889
kondziet <konrad.dabrowski54@gmail.com>
2023-08-01 21:59:32 +0200

    Create custom axios hook for attaching bearer token to private requests

---
+++ react-frontend/src/hooks/usePrivateClientRequest.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import useAuthenticationContext from "./useAuthentication";
+import { privateClientRequest } from "../api/clientRequest";
+
+const usePrivateClientRequest = () => {
+    const { authentication } = useAuthenticationContext();
+
+    useEffect(() => {
+        const requestIntercept = privateClientRequest.interceptors.request.use(
+            config => {
+                if (!config.headers.Authorization) {
+                    config.headers.Authorization = `Bearer ${authentication?.accessToken}`;
+                }
+                return config;
+            }, (error) => Promise.reject(error)
+        );
+
+        const cleanup = () => {
+            privateClientRequest.interceptors.request.eject(requestIntercept);
+        };
+
+        return cleanup;
+
+    }, [authentication]);
+
+    return privateClientRequest;
+}
+
+export default usePrivateClientRequest;
