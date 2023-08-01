6cd191b1d72e5f4a0a6f58d6d8dcd46035fa0d8a
kondziet <konrad.dabrowski54@gmail.com>
2023-08-01 22:02:18 +0200

    Specify new auto generated method

--- spring-backend/src/main/java/pl/kondziet/springbackend/repository/ChatRoomRepository.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/repository/ChatRoomRepository.java
@@ -2,6 +2,9 @@ package pl.kondziet.springbackend.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import pl.kondziet.springbackend.model.entity.ChatRoom;
+import reactor.core.publisher.Flux;
 
 public interface ChatRoomRepository extends ReactiveMongoRepository<ChatRoom, String> {
+
+    Flux<ChatRoom> findByUserIdsContaining(String userId);
 }
