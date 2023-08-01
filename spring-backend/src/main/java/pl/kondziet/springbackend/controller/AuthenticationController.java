c1609c4ab55b74c0c1e454097c502fc58d6959ef
kondziet <konrad.dabrowski54@gmail.com>
2023-08-01 22:02:02 +0200

    Add CrossOrigin annotation for AuthenticationController

--- spring-backend/src/main/java/pl/kondziet/springbackend/controller/AuthenticationController.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/controller/AuthenticationController.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.kondziet.springbackend.exception.UserAlreadyExistsException;
 import pl.kondziet.springbackend.model.dto.LoginRequest;
 import pl.kondziet.springbackend.model.dto.LoginResponse;
@@ -17,6 +14,7 @@ import pl.kondziet.springbackend.service.AuthenticationService;
 import reactor.core.publisher.Mono;
 
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/authentication")
 public class AuthenticationController {
