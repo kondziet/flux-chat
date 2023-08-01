b945dac4a0ffd1d9780bea281bd5166d245f2c64
kondziet <konrad.dabrowski54@gmail.com>
2023-08-01 22:02:53 +0200

    Fix typo

--- spring-backend/src/main/java/pl/kondziet/springbackend/model/dto/RegisterRequest.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/model/dto/RegisterRequest.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @AllArgsConstructor
 public class RegisterRequest {
 
+    private String username;
     private String email;
     private String password;
-    private String username;
 }
