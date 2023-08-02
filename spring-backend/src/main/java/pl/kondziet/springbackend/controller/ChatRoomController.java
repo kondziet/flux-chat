899380cb59b8c4e3adf67e986d090b479d0c91c8
kondziet <konrad.dabrowski54@gmail.com>
2023-08-03 00:42:30 +0200

    Create new ChatRoomController

---
+++ spring-backend/src/main/java/pl/kondziet/springbackend/controller/ChatRoomController.java
@@ -0,0 +1,23 @@
+package pl.kondziet.springbackend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/chatroom")
+public class ChatRoomController {
+
+    @PostMapping("/")
+    public Mono<ResponseEntity<String>> createNewChatRoom() {
+        return ReactiveSecurityContextHolder.getContext()
+                .map(SecurityContext::getAuthentication)
+                .map(Principal::getName)
+                .doOnNext(System.out::println)
+                .flatMap(auth -> Mono.just(ResponseEntity.ok(auth)));
+    }
+}
