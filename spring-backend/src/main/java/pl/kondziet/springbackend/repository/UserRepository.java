fe7440dfec764d998e542ac8dd3d121fb1b7d549
kondziet <konrad.dabrowski54@gmail.com>
2023-08-03 15:37:39 +0200

    Specify method for retrieving User by email

--- spring-backend/src/main/java/pl/kondziet/springbackend/repository/UserRepository.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/repository/UserRepository.java
@@ -1,13 +1,12 @@
 package pl.kondziet.springbackend.repository;
 
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import pl.kondziet.springbackend.model.entity.User;
 import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
-
+    Mono<User> findUserByEmail(String email);
     Mono<UserDetails> findByEmail(String email);
     Mono<UserDetails> findByUsername(String username);
 }
